3750ecb5ee3cdfc3aa8f912c91108219ae20cd74
Utkarshp23 <utkarshpawar2332@gmail.com>
2021-07-24 16:15:14 +0530

    Heading & id of task updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { nanoid } from "nanoid";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
@@ -7,28 +8,30 @@ import FilterButton from "./components/FilterButton";
 
 function App(props) {
 
-   const[tasks, setTasks]=useState(props.tasks);
+  const [tasks, setTasks] = useState(props.tasks);
 
-   function addTask(name) {
-    const newTask = { id: "id", name: name, completed: false };
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
   }
 
   const taskList = tasks.map(task => (
     <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
   ));
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoSpark</h1>
-      <Form addTask={addTask}/>
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">
-        3 tasks remaining
-      </h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
