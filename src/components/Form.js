2a5afca90793f19154b7ed2fbfeb61ec12544f81
Utkarshp23 <utkarshpawar2332@gmail.com>
2021-07-24 14:02:01 +0530

    onSubmit event handler added to form with handleSubmit fun which prevets default action of submit & use the addTask callback prop

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,32 @@
 import React from "react";
 
-function Form(props){
+
+
+function Form(props) {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.addTask("Say Hello!");
+  }
+
   return (
-    <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
   );
 }
 
